4c5b0f7f2e81d6f20590ef316c3a9876f962e816
Miguel <krhono62@gmail.com>
2021-10-13 15:35:38 -0500

    listo

---
+++ src/BD/Firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app';
+const firebaseConfig = {
+  apiKey: "AIzaSyBwVp5Q1qN4xVMVOx6sHqseP4T_mFkFoso",
+  authDomain: "crud-fbf18.firebaseapp.com",
+  projectId: "crud-fbf18",
+  storageBucket: "crud-fbf18.appspot.com",
+  messagingSenderId: "560541652720",
+  appId: "1:560541652720:web:8802a40e829fd03c869435"
+};
+  
+  // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
